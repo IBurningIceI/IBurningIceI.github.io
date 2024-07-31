c5a4192246a1697ced259e08388db5ac2060583e
IBurningIceI <willboarder1@gmail.com>
2024-07-31 00:20:48 -0400

    Assignment 4 - Part 1
    Finished Part 1

--- assignment4/part1/main.js
+++ assignment4/part1/main.js
@@ -15,15 +15,15 @@ function randomValueFromArray(array){
 }
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-let insertX = "Willy the Goblin\n" +
-    "Big Daddy\n" +
-    "Father Christmas";
-let insertY = "the soup kitchen\n" +
-    "Disneyland\n" +
-    "the White House";
-let insertZ = "spontaneously combusted\n" +
-    "melted into a puddle on the sidewalk\n" +
-    "turned into a slug and crawled away";
+let insertX = ["Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"];
+let insertY = ["the soup kitchen",
+    "Disneyland",
+    "the White House"];
+let insertZ = ["spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"];
 
 randomize.addEventListener('click', result);
 
@@ -37,19 +37,21 @@ function result() {
 
     newStory =  newStory.replaceAll(":insertx:", xItem);
     newStory = newStory.replace(":inserty:", yItem);
-    newStory = newStory.replace(":insertz:", zItem)
+    newStory = newStory.replace(":insertz:", zItem);
 
     if (customName.value !== '') {
         const name = customName.value;
-        newStory = newStory.replace("Bob", name)
+        newStory = newStory.replace("Bob", name);
     }
 
     if (document.getElementById("uk").checked) {
-        const weight = Math.round(300 / 14);
-        const temperature = Math.round(94);
+        const weight = Math.round(300 / 14) + " stone";
+        const temperature = Math.round((94 - 32) * (5/9)) + " centigrade";
+        newStory = newStory.replace('94 fahrenheit', temperature);
+        newStory = newStory.replace('300 pounds', weight);
 
     }
 
-    story.textContent =;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
