bfa2a9d1ed3e603357b5a5800f2e3751cc5b9053
IBurningIceI <willboarder1@gmail.com>
2024-08-01 04:17:43 -0400

    Assignment 4 - Part 2
    Added file names array and alt array, for loop to assign them. Plus button to switch them to main

--- assignment4/part2/gallery-start/gallery-start/main.js
+++ assignment4/part2/gallery-start/gallery-start/main.js
@@ -11,14 +11,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+
+const imageAlts = ["An image depicting someones eye.", "An image depicting a rock.", "An image depicting a set of flowers.", "An image depicting a rock.",
+"An image depiction ancient egypt.", "An image depiction a butterfly."]
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for(const image of imageFilenames){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', imageAlts[image])
+    newImage.addEventListener("click", () => {
+        displayedImage.setAttribute('src', newImage.getAttribute('src'))
+        displayedImage.setAttribute('alt', newImage.getAttribute('alt'))
+    })
+    thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
